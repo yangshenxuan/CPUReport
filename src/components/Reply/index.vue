59ed3d1f50d0c53e10b7539bdc4c2d591553cf1a
ggceslie <cesliezgt@163.com>
2022-03-22 18:00:39 +0800

    reply html

--- src/components/Reply/index.vue
+++ src/components/Reply/index.vue
@@ -1,20 +1,50 @@
 <template>
   <div class="replybox">
     <div class="title" v-if="!exportShow">
-      <p>理论考核<span>考核人数:{{num}}人</span></p>
+      <p>
+        理论考核<span>考核人数:{{ num }}人</span>
+      </p>
     </div>
     <div class="time" v-if="exportShow">
       <div style="display：flex;">
-        <el-select v-model="listQuery.userId"></el-select>
+        <el-select
+          v-model="listQuery.userId"
+          filterable
+          :filter-method="handleFilter"
+        >
+          <el-option label="全部" :value="-1"></el-option>
+          <div v-infinite-scroll="load" style="overflow-y: auto">
+            <el-option
+              v-for="item in userList"
+              :label="item.name"
+              :value="item.id"
+              :key="item.id"
+            ></el-option></div
+        ></el-select>
+        <el-select
+          v-model="listQuery.tpId"
+          style="margin-left: 20px"
+          filterable
+          :filter-method="handlePaperFilter"
+        >
+          <el-option label="全部" :value="-1"></el-option>
+          <div v-infinite-scroll="loadPaper" style="overflow-y: auto">
+            <el-option
+              v-for="item in paperList"
+              :label="item.name"
+              :value="item.id"
+              :key="item.id"
+            ></el-option></div
+        ></el-select>
+        <el-button type="primary"
+                   style="margin-left:20px"
+                   @click="getReplyList()">查询</el-button>
       </div>
+      <el-button @click="handleExport">成绩导出</el-button>
     </div>
   </div>
 </template>
 <script>
-export default {
-  
-}
+export default {};
 </script>
-<style lang="scss">
-  
-</style>
+<style lang="scss"></style>
